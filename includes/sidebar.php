e7669e8c9a45946485f20473bbc3761b38c8b473
Baher Ramzy <baherramzy@gmail.com>
2016-12-10 21:09:00 -0500

    Add WIP nav sidebar, pending collapse toggle and brand colors

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,2 +1,20 @@
-<aside>
-</aside>
+<div id="sidebar-wrapper">
+	<ul class="sidebar-nav">
+		<li>
+			<a href="#">Baher Ramzy</a>
+		</li>
+		<li>
+			<a href="#">Link 1</a>
+		</li>
+		<li>
+			<a href="#">Link 1</a>
+		</li>
+		<li>
+			<a href="#">Link 1</a>
+		</li>
+		<li>
+			<a href="#">Link 1</a>
+		</li>
+	</ul>
+</div>
+<!-- /#sidebar-wrapper -->
