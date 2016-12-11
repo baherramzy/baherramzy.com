e9c0b2e7684271337de29dbabd0b92305eb81c8a
Baher Ramzy <baherramzy@gmail.com>
2016-12-10 21:06:16 -0500

    Modularize page components

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Baher Ramzy | Portfolio</title>
-</head>
-<body>
-	<h1>Hello world!</h1>
-</body>
-</html>
+<?php require('includes/header.php'); ?>
+
+<?php require('includes/sidebar.php'); ?>
+
+<div id="page-content-wrapper">
+	<h1>This is a test</h1>
+</div>
+
+<?php require('includes/footer.php'); ?>
