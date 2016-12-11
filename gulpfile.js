69bb27ad348e1ee8ba54fd601bdf507dee6deed9
Baher Ramzy <baherramzy@gmail.com>
2016-12-10 21:02:35 -0500

    Add & configure gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 // Gulp
 var gulp = require('gulp'),
-
-// Gulp plugins
-minifyCss = require('gulp-minify-css'),
-sass = require('gulp-sass'),
-uglify = require('gulp-uglify'),
-rename = require('gulp-rename'),
-concat = require('gulp-concat'),
-autoprefixer = require('gulp-autoprefixer')
+	// Gulp plugins
+	minifyCss = require('gulp-minify-css'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	autoprefixer = require('gulp-autoprefixer'),
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber'),
+	promise = require('es6-promise').polyfill();
 
 gulp.task('styles', function() {
 	return gulp.src(['./sass/**/*.scss'])
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
+		.pipe(sass())
 		.pipe(minifyCss())
 		.pipe(concat('style.css'))
 		.pipe(autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
@@ -21,14 +24,13 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-	return gulp.src(['.js/scripts.js'])
+	return gulp.src(['./js/scripts.js', './js/bootstrap.js'])
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%- error.message %>')
 		}))
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./js'))
-		.pipe(reload({stream:true}));
 });
 
 gulp.task('watch', function() {
