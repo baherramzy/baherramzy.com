37fbe724aa385b56599ee5be5216889ba0297fa9
Baher Ramzy <baherramzy@gmail.com>
2016-12-19 05:59:53 -0500

    Add FontAwesome + pages

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,19 +1,19 @@
 <div id="sidebar-wrapper">
 	<ul class="sidebar-nav">
-		<li>
+		<li class="brand">
 			<a href="#">Baher Ramzy</a>
 		</li>
 		<li>
-			<a href="#">Link 1</a>
+			<a href="about">About</a>
 		</li>
 		<li>
-			<a href="#">Link 1</a>
+			<a href="projects">Projects</a>
 		</li>
 		<li>
-			<a href="#">Link 1</a>
+			<a href="resume">Resume</a>
 		</li>
 		<li>
-			<a href="#">Link 1</a>
+			<a href="contact">Contact</a>
 		</li>
 	</ul>
 </div>
