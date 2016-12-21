80ac33530ee5ac6c8d2e6ff99edc648b2521ef14
Baher Ramzy <baherramzy@gmail.com>
2016-12-21 13:01:11 -0500

    Add main header, social links

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,50 @@
 </head>
 <body>
 
-<header>
-	<!-- Menu toggle button -->
-	<div class="menu-toggle-container">
-		<i class="menu-toggle fa fa-bars fa-2x"></i>
-	</div>
-</header>
+	<header>
+		<div class="row">
+			<!-- Menu toggle button -->
+			<div class="col-sm-2">
+				<div class="menu-toggle-container">
+					<i class="menu-toggle fa fa-bars fa-2x"></i>
+				</div>
+			</div>
+
+			<div class="col-sm-6">
+				<div class="hero-name">
+					<p>Baher Ramzy</p>
+				</div>
+			</div>
+
+			<div class="col-sm-4">
+				<div class="social">
+					<ul class="social-links">
+						<li>
+							<a href="https://github.com/baherramzy" class="fa-stack fa-lg">
+								<i class="fa fa-circle-thin fa-stack-2x"></i>
+								<i class="fa fa-github fa-stack-1x"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://linkedin.com/in/baherramzy" class="fa-stack fa-lg">
+								<i class="fa fa-circle-thin fa-stack-2x"></i>
+								<i class="fa fa-linkedin fa-stack-1x"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://twitter.com/BaherRamzy" class="fa-stack fa-lg">
+								<i class="fa fa-circle-thin fa-stack-2x"></i>
+								<i class="fa fa-twitter fa-stack-1x"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://instagram.com/baherramzy" class="fa-stack fa-lg">
+								<i class="fa fa-circle-thin fa-stack-2x"></i>
+								<i class="fa fa-instagram fa-stack-1x"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(function() {
+	// Open all links in new tabs
+	// (Some might call this wrong and hacky)
+	// (If that's the case, I don't want to be right!)
+	$('a').attr('target', '_blank');
+
 	// Toggle the sidebar nav menu
 	$('.menu-toggle-container .fa.fa-bars').on('click', function() {
 		$('#sidebar-wrapper').addClass('open');
